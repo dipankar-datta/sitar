38b6b0d7e48abc84abe7b71b6a5698a5c2f45427
Dipankar Datta <dattapro5@gmail.com>
2021-04-04 17:50:57 +0530

    Implemented new features for shelf.

--- src/shelf/shelf-manager.ts
+++ src/shelf/shelf-manager.ts
@@ -9,9 +9,18 @@ export interface ShelfData {
 
 export type ShelfEventHandler = (data: ShelfData) => void;
 
+export type ShelfEventFilter = (data: ShelfData) => any | null;
+
 export interface ShelfEventSubscription {
   subscriptionId: string;
-  callback: ShelfEventHandler;
+  callback?: ShelfEventHandler;
+  eventFilter?: ShelfEventFilter;
+  filteredCallback?: (filteredData: any) => any;
+}
+
+export interface ShelfEventFilterArgs {
+  subscriptionKey: string;
+  eventFilter: ShelfEventFilter;
 }
 
 export interface ShelfSubscription {
@@ -40,6 +49,26 @@ export const getShelfData = (subscriptionKey: string): any => {
   return ShelfManager.getShelfData(subscriptionKey);
 };
 
+export const subscribeShelfFilter = (
+  filterArgs: ShelfEventFilterArgs,
+  filteredCallback: (data: any) => any,
+  triggerNow?: boolean,
+): ShelfSubscription => {
+  if (!filterArgs) {
+    throw new Error('Invalid Shelf filter arguments.');
+  }
+  return ShelfManager.subscribeShelfFilter(
+    filterArgs.subscriptionKey,
+    filterArgs.eventFilter,
+    filteredCallback,
+    triggerNow,
+  );
+};
+
+export const clearShelf = (subscriptionKey: string): boolean => {
+  return ShelfManager.clearShelf(subscriptionKey);
+}
+
 class ShelfManager {
   private static shelf: Map<string, ShelfSubscriptionData> = new Map();
 
@@ -64,7 +93,14 @@ class ShelfManager {
 
       shelf.subscriptions.forEach((eventSub: ShelfEventSubscription) => {
         if (shelf) {
-          eventSub.callback(shelf.data);
+          if (eventSub.eventFilter && eventSub.filteredCallback) {
+            const filteredData = eventSub.eventFilter(shelf.data);
+            if (filteredData) {
+              eventSub.filteredCallback(filteredData);
+            }
+          } else if (eventSub.callback) {
+            eventSub.callback(shelf.data);
+          }
         }
       });
     }
@@ -101,6 +137,43 @@ class ShelfManager {
     return { id, unsubscribeShelf: () => this.unsubscribeShelf(subscriptionKey, id) };
   }
 
+  static subscribeShelfFilter(
+    subscriptionKey: string,
+    eventFilter: ShelfEventFilter,
+    filteredCallback: (filteredData: any) => any,
+    triggerNow = false,
+  ): ShelfSubscription {
+    const id = uuid();
+    if (subscriptionKey && eventFilter && filteredCallback) {
+      const subscriptionData = this.shelf.get(subscriptionKey);
+      if (subscriptionData) {
+        subscriptionData.subscriptions.set(id, { subscriptionId: id, filteredCallback, eventFilter });
+
+        if (triggerNow) {
+          if (subscriptionData.data) {
+            const filteredData = eventFilter(subscriptionData.data);
+            if (filteredData) filteredCallback(filteredData);
+          }
+        }
+      } else {
+        const subsData: ShelfSubscriptionData = {
+          data: {
+            key: subscriptionKey,
+            current: null,
+            previous: null,
+          },
+          subscriptions: new Map(),
+        };
+        subsData.subscriptions.set(id, { subscriptionId: id, filteredCallback, eventFilter });
+        this.shelf.set(subscriptionKey, subsData);
+      }
+    } else {
+      throw new Error('Invalid subscription key or eventFilter or callback.');
+    }
+
+    return { id, unsubscribeShelf: () => this.unsubscribeShelf(subscriptionKey, id) };
+  }
+
   static getShelfData(subscriptionKey: string): any {
     const subsData = this.shelf.get(subscriptionKey);
     return subsData ? _.cloneDeep(subsData.data) : null;
@@ -110,4 +183,22 @@ class ShelfManager {
     const subsData = this.shelf.get(subscriptionKey);
     return subsData ? subsData.subscriptions.delete(subscriptionId) : false;
   }
+
+  static clearShelf(subscriptionKey: string): boolean {
+    if (subscriptionKey) {
+      const subData = this.shelf.get(subscriptionKey);
+      if (subData) {
+        subData.subscriptions.forEach((eventSub: ShelfEventSubscription) => {
+          if (eventSub.callback) {
+            eventSub.callback({current: null, previous: null, key: subscriptionKey});
+          } else if (eventSub.filteredCallback) {
+            eventSub.filteredCallback(null);
+          }
+        });
+      }
+  
+      return this.shelf.delete(subscriptionKey);
+    }
+    return false;    
+  } 
 }
--- src/tests/shelf/shelf-manager.spec.ts
+++ src/tests/shelf/shelf-manager.spec.ts
@@ -1,4 +1,4 @@
-import { setShelf, getShelfData, subscribeShelf, ShelfData } from '../../shelf/shelf-manager';
+import { setShelf, getShelfData, subscribeShelf, ShelfData, subscribeShelfFilter, clearShelf } from '../../shelf/shelf-manager';
 
 describe('Test Shelf', () => {
   test('Should set data in Shelf', () => {
@@ -139,5 +139,193 @@ describe('Test Shelf', () => {
       },
       true,
     );
+  });
+
+  test('Should test shelf filter subscription', (done) => {
+    const testKey = 'SHELF_TEST_KEY_7';
+    const testData = { alfa: 10, beta: 20 };
+    setShelf(testKey, testData);
+    const shelfFilterArgs = {
+      subscriptionKey: testKey,
+      eventFilter: (data: ShelfData) => {
+        if (data.current.beta !== data.previous.beta) {
+          return { shelfData: data, filterData: { beta: data.current.beta } };
+        }
+        return null;
+      },
+    };
+
+    const sub1 = subscribeShelfFilter(shelfFilterArgs, (data: any) => {
+      expect(data).not.toBeNull();
+      if (data) {
+        expect(data.filteredData).not.toBeNull();
+        if (data.filteredData) {
+          expect(data.filteredData.beta).toBe(30);
+        }
+        expect(data.shelfData).not.toBeNull();
+      }
+    });
+
+    const sub2 = subscribeShelf(testKey, (data: ShelfData) => {
+      expect(data).not.toBeNull();
+    });
+
+    setShelf(testKey, { alfa: 10, beta: 30 });
+
+    const doUnsub = async () => {
+      expect(sub1.unsubscribeShelf()).toBeTruthy();
+      expect(sub1.unsubscribeShelf()).toBeFalsy();
+      expect(sub2.unsubscribeShelf()).toBeTruthy();
+      expect(sub2.unsubscribeShelf()).toBeFalsy();
+      done();
+    };
+    doUnsub();
+  });
+
+  test('Should test shelf filter subscription with triggerNow', (done) => {
+    const testKey = 'SHELF_TEST_KEY_8';
+    const testData = { alfa: 10, beta: 20 };
+    setShelf(testKey, testData);
+    setShelf(testKey, { alfa: 10, beta: 30 });
+    const shelfFilterArgs = {
+      subscriptionKey: testKey,
+      eventFilter: (data: ShelfData) => {
+        if (data.current.beta !== data.previous.beta) {
+          return { shelfData: data, filterData: { beta: data.current.beta } };
+        }
+        return null;
+      },
+    };
+
+    const sub = subscribeShelfFilter(
+      shelfFilterArgs,
+      (data: any) => {
+        expect(data).not.toBeNull();
+        if (data) {
+          expect(data.filteredData).not.toBeNull();
+          if (data.filteredData) {
+            expect(data.filteredData.beta).toBe(30);
+          }
+          expect(data.shelfData).not.toBeNull();
+        }
+      },
+      true,
+    );
+
+    const doUnsub = async () => {
+      expect(sub.unsubscribeShelf()).toBeTruthy();
+      expect(sub.unsubscribeShelf()).toBeFalsy();
+      done();
+    };
+    doUnsub();
+  });
+
+  test('Should test shelf filter with subscription before setting data', (done) => {
+    const testKey = 'SHELF_TEST_KEY_9';
+    const testData = { alfa: 10, beta: 20 };
+
+    const shelfFilterArgs = {
+      subscriptionKey: testKey,
+      eventFilter: (data: ShelfData) => {
+        if (data.previous == null && data.current.beta !== null) {
+          return { shelfData: data, filterData: { beta: data.current.beta } };
+        }
+        return null;
+      },
+    };
+
+    const sub = subscribeShelfFilter(shelfFilterArgs, (data: any) => {
+      expect(data).not.toBeNull();
+      if (data) {
+        expect(data.filteredData).not.toBeNull();
+        if (data.filteredData) {
+          expect(data.filteredData.beta).toBe(30);
+        }
+        expect(data.shelfData).not.toBeNull();
+      }
+    });
+
+    setShelf(testKey, testData);
+
+    const doUnsub = async () => {
+      expect(sub.unsubscribeShelf()).toBeTruthy();
+      expect(sub.unsubscribeShelf()).toBeFalsy();
+      done();
+    };
+    doUnsub();
+  });
+
+  test('Should test shelf filter for invalid subscription arguments', () => {
+    try {
+      subscribeShelfFilter(null as any, null as any);
+    } catch (err: any) {
+      expect(err).toBeDefined();
+      expect(err.message).toBe('Invalid Shelf filter arguments.');
+    }
+  });
+
+  test('Should test shelf filter for invalid subscription key', () => {
+    try {
+      subscribeShelfFilter({ subscriptionKey: null as any, eventFilter: (data) => {} }, (data) => {});
+    } catch (err: any) {
+      expect(err).toBeDefined();
+      expect(err.message).toBe('Invalid subscription key or eventFilter or callback.');
+    }
+  });
+
+  test('Should test shelf filter for invalid subscription eventfilter', () => {
+    try {
+      subscribeShelfFilter({ subscriptionKey: 'TEST_KEY', eventFilter: null as any }, (data) => {});
+    } catch (err: any) {
+      expect(err).toBeDefined();
+      expect(err.message).toBe('Invalid subscription key or eventFilter or callback.');
+    }
+  });
+
+  test('Should test shelf filter for invalid subscription callback', () => {
+    try {
+      subscribeShelfFilter({ subscriptionKey: 'TEST_KEY', eventFilter: (data) => {} }, null as any);
+    } catch (err: any) {
+      expect(err).toBeDefined();
+      expect(err.message).toBe('Invalid subscription key or eventFilter or callback.');
+    }
+  });
+
+  test('Should test clearing shelf', done => {
+    const testKey = 'SHELF_TEST_KEY_10';
+    const testData = { alfa: 10, beta: 20 };
+    setShelf(testKey, testData);
+    const shelfFilterArgs = {
+      subscriptionKey: testKey,
+      eventFilter: (data: ShelfData) => {
+        return data;
+      },
+    };
+
+    subscribeShelfFilter(shelfFilterArgs, (data: any) => {
+      expect(data).toBeNull();
+      if (data) {
+        expect(data.filteredData).not.toBeNull();
+        if (data.filteredData) {
+          expect(data.filteredData.beta).toBe(30);
+        }
+        expect(data.shelfData).not.toBeNull();
+      }
+    });
+
+    subscribeShelf(testKey, (data: ShelfData) => {
+      expect(data).not.toBeNull();
+      if (data) {
+        expect(data.current).toBeNull();
+        expect(data.previous).toBeNull();
+        expect(data.key).toBe(testKey);
+      }
+    });
+
+    expect(clearShelf(testKey)).toBeTruthy();
+    expect(clearShelf(testKey)).toBeFalsy();
+    expect(clearShelf(null as any)).toBeFalsy();
+    done();
+
   });
 });
